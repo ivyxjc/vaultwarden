f392d165ffe5eaee58af0ab6c03afb24f273d29b
ivyxjc <ivyxjc1994@hotmail.com>
2025-02-14 13:38:11 +0800

    fix!

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,6 @@ use tokio::signal::unix::SignalKind;
 
 use lambda_web::{is_running_on_lambda, launch_rocket_on_lambda};
 
-
 #[macro_use]
 mod error;
 mod api;
@@ -618,10 +617,10 @@ async fn launch_rocket(pool: db::DbPool, extra_debug: bool) -> Result<(), Error>
         });
     }
 
-    if is_running_on_lambda(){
-        let _ = launch_rocket_on_lambda(instance).await;
-    }else{
-        let _ = instance.launch().await?;
+    if is_running_on_lambda() {
+        drop(launch_rocket_on_lambda(instance).await);
+    } else {
+        drop(instance.launch().await?);
     }
 
     info!("Vaultwarden process exited!");
